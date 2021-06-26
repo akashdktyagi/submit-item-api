8acbf61af97c0ea12b11f1bd9e616d3be7c0d0ea
akashtyagi <akashdktyagi@gmail.com>
2021-06-27 00:05:17 +0800

    first version

---
+++ src/main/java/com/yantraCloudApp/submititemapi/SubmitItemApiApplication.java
@@ -0,0 +1,13 @@
+package com.yantraCloudApp.submititemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmitItemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmitItemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yantraCloudApp/submititemapi/SubmitItemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yantraCloudApp.submititemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmitItemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
